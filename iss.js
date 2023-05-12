13c0b8962d0c1c0ae1d7ca383a04e3cd4816f025
bryanc.n <bryan.chuinkam@gmail.com>
2023-05-12 13:04:30 +0000

    added fetchISSFlyOverTimes functionality

--- iss.js
+++ iss.js
@@ -1,13 +1,3 @@
-// It will contain most of the logic for fetching the data from each API endpoint.
-
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
-*/
 const request = require('request');
 
 const fetchMyIP = function(callback) {
@@ -48,4 +38,26 @@ const fetchCoordsByIP = function(ip, callback) {
 };
 
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+
+  request(`https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`, function(error, response, body) {
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching ISS pass times: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+
+    const flyOverTimes = JSON.parse(body).response;
+    callback(error, flyOverTimes);
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes};
